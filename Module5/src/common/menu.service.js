8fe6c1fbf085509fb512cad5d11cc251de29de09
Alex Cusenza <144127539+Alex92C@users.noreply.github.com>
2023-12-12 15:23:27 +0100

    Update menu.service.js

--- Module5/src/common/menu.service.js
+++ Module5/src/common/menu.service.js
@@ -8,17 +8,7 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
-  service.user = {};
 
-  service.saveUser = function(user) {
-    service.user = angular.copy(user);
-    console.log(service.user);
-  }
-
-  service.getUser = function() {
-    return service.user;
-  }
-  
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
       return response.data;
@@ -27,20 +17,72 @@ function MenuService($http, ApiPath) {
 
 
   service.getMenuItems = function (category) {
-    var config = {};
-    if (category) {
-      config.params = {'category': category};
-    }
-
-    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+    return $http.get(ApiPath + '/menu_items/' + category + '.json').then(function (response) {
       return response.data;
     });
   };
 
-  service.getFavoriteDish = function(categoryShortName, menuNumber) {
-    return $http.get(ApiPath + '/menu_items/' + categoryShortName + '/menu_items/' + menuNumber + '.json');
-}
+  /**Private function for splitting input favMenu string for HTTP request.
+  */
+  let splitFavMenu = function (favMeal) {
+    let splitArray = favMeal.split("");
+    let result = {shortCategory: "", itemNumber: ""};
+    
+    /**Adds letters to the input category shortname, adds digits to the input 
+     * category item number.*/
+    splitArray.forEach(element => {
+      if (isNaN(element)){
+        result.shortCategory += element;
+      } else {
+        result.itemNumber += element;
+      }
+    });
 
+    return result;
+  }
+
+  /**Move updateInfo menuItem verification here. This will return bool instead
+   * for that service and anything *only* wanting verification services to react
+   * accordingly.
+  */
+  service.verifyMenuItem = (currentInput)=>{
+    console.log("Verifying current input!");
+  }
+
+  /**First split favMeal input into an HTTP request, then make request.
+   * If request data returns a NULL, then return false (Item does not exist),
+   * otherwise return true (Item exists). This will affect the controller's response.
+  */
+  service.userInfo = {};
+  service.updateInfo = (newInfo) => {
+    let inputRequest = splitFavMenu(newInfo.favMeal);
+
+    console.log("Checking if menu item exists!");
+
+    return $http({
+      method:'GET',
+      url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/" +
+        inputRequest.shortCategory +
+        "/menu_items/" +
+        inputRequest.itemNumber +
+        ".json"
+    })
+    .then(response=>{
+      console.log(response);
+
+      if(response.data == null){
+        console.log("Whoops, menu item doesn't exist!");
+        return false;
+      }
+      else{
+        console.log("Item confirmed! ");
+        service.userInfo = newInfo;
+        service.userInfo.favMealItem = response.data;
+        service.userInfo.splitFav=inputRequest;
+        return true;
+      }
+    });
+  };
 }
 
 
